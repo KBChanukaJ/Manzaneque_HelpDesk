a77542aface86c2b178877ed13eb4217cc11394c
KBCHANUKAJ <chanukakbc@gmail.com>
2023-12-23 02:18:22 +0530

    Registration

---
+++ Source_Code/register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css"> <!-- Your custom CSS file -->
+    <title>Helpdesk Operator Registration</title>
+</head>
+<body>
+
+<?php include('Connection.php'); ?>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="text-center">Helpdesk Operator Registration</h3>
+                </div>
+                <div class="card-body">
+                    <form action="register_process.php" method="post">
+                        <div class="form-group">
+                            <label for="operatorID">Operator ID:</label>
+                            <input type="text" class="form-control" id="operatorID" name="operatorID" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="operatorName">Operator Name:</label>
+                            <input type="text" class="form-control" id="operatorName" name="operatorName" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
---
+++ Source_Code/register_process.php
@@ -0,0 +1,53 @@
+<?php
+include('Connection.php');
+
+// Function to validate if a string is a number
+function isNumber($string) {
+    return is_numeric($string);
+}
+
+// Function to display an alert with redirection
+function showAlert($message, $redirectUrl) {
+    echo "<script>alert('$message'); window.location.href = '$redirectUrl';</script>";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve form data
+    $operatorID = $_POST['operatorID'];
+    $operatorName = $_POST['operatorName'];
+    $password = $_POST['password'];
+
+    // Validate Operator ID is a number
+    if (!isNumber($operatorID)) {
+        showAlert("Operator ID must be a number.", 'register.php');
+    }
+    // Validate password length
+    elseif (strlen($password) < 8) {
+        showAlert("Password must be at least 8 characters.", 'register.php');
+    } else {
+        // Check if OperatorID is already registered
+        $checkQuery = "SELECT OperatorID FROM HelpdeskOperators WHERE OperatorID = '$operatorID'";
+        $result = $conn->query($checkQuery);
+
+        if ($result->num_rows > 0) {
+            showAlert("Operator ID is already registered.", 'register.php');
+        } else {
+            // Password hash
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+
+            // Insert data into the HelpdeskOperators table
+            $insertQuery = "INSERT INTO HelpdeskOperators (OperatorID, OperatorName, Password) VALUES ('$operatorID', '$operatorName', '$hashedPassword')";
+
+            // Execute the query
+            if ($conn->query($insertQuery) === TRUE) {
+                showAlert("Registration successful!", 'register.php');
+            } else {
+                showAlert("Error: Registration failed. " . $conn->error, 'register.php');
+            }
+        }
+
+        // Close the database connection
+        $conn->close();
+    }
+}
+?>
