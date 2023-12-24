a025d97be16817414550684b83c5c0384d853620
KBCHANUKAJ <chanukakbc@gmail.com>
2023-12-24 20:22:02 +0530

    done all the changes

--- Source_Code/view_personnel.php
+++ Source_Code/view_personnel.php
@@ -47,7 +47,7 @@ if (!$result) {
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <span class="navbar-brand">Your Company Name</span>
+        <span class="navbar-brand">Manzaneque Limited</span>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
